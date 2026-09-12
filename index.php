8d4536318fad106376dd69d8a0d8a1602581ebba
OmarBenedettoGianni <omarbenedetto200012@gmail.com>
2026-09-12 17:03:32 +0200

    implementato ciclo

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
 <?php
+
+$numbers =[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,20];
+$counter = 0;
+$total = 0;
+
+foreach($numbers as $number){
+    if($number %2 == 0){
+        $total += $number;
+        $counter ++;
+    }
+}
+
+echo $total/$counter;
